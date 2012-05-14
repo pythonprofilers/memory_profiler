b7e2cddeca38ab506bcef1a8260ceccce9496838
Victor <binary.digit.bucket@gmail.com>
2012-05-14 07:00:02 +0300

    Fix UserWarning that occurs in Python 3

--- memory_profiler.py
+++ memory_profiler.py
@@ -141,7 +141,7 @@ class LineProfiler:
         """ Record line profiling information for the given Python function.
         """
         try:
-            code = func.func_code
+            code = func.__code__
         except AttributeError:
             import warnings
             warnings.warn("Could not extract a code object for the object {!r}"
