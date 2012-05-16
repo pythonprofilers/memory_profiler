a9b5c4799d47deb89006a018c67c999e87a7cfb1
Fabian Pedregosa <fabian@fseoane.net>
2012-05-16 13:51:56 +0200

    update test

--- test/test_func.py
+++ test/test_func.py
@@ -2,12 +2,9 @@
 @profile
 def test_1():
     # .. will be called twice ..
-    a = 2.
-    b = 3
     c = {}
     for i in range(1000):
         c[i] = 2
-    c[0] = 2.
 
 if __name__ == '__main__':
     test_1()
