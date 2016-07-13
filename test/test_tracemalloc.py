fc44641d566e58f3274421eb6a1c10718dd67e64
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-07-13 04:49:07 +0300

    TEST added test for ``tracemalloc``

--- test/test_tracemalloc.py
+++ test/test_tracemalloc.py
@@ -1,29 +1,20 @@
-import pytest
-import tracemalloc
 from io import StringIO
 from time import sleep
 
 from memory_profiler import profile
-import memory_profiler
 
-tracemalloc.start()
+try:
+    import tracemalloc
+    has_tracemalloc = True
+except ImportError:
+    has_tracemalloc = False
+
 output = StringIO()
 
 # allowable error in MB
 EPSILON = 0.0001
 
-memory_profiler._backend = 'tracemalloc'
 
-
-@pytest.mark.parametrize('test_input,expected', [
-    (100, 0.00012302398681640625),
-    (1000, 0.0009813308715820312),
-    (10000, 0.009564399719238281),
-    (100000, 0.09539508819580078),
-    (1000000, 0.9537019729614258),
-    (10000000, 9.536770820617676),
-    (100000000, 95.36745929718018),
-])
 def test_memory_profiler(test_input, expected):
     mem_prof(test_input)
     inc, dec = parse_mem_prof()
@@ -31,7 +22,7 @@ def test_memory_profiler(test_input, expected):
     assert abs(inc - expected) <= EPSILON, 'inc = {}, size = {}, err = {}'.format(inc, expected, abs(inc - expected))
 
 
-@profile(stream=output, precision=6)
+@profile(stream=output, precision=6, backend='tracemalloc')
 def mem_prof(n):
     a = bytearray(n)
     del a
@@ -45,3 +36,17 @@ def parse_mem_prof():
         return float(s.split()[3])
 
     return f(text[-6]), -f(text[-5])
+
+if __name__ == '__main__':
+    if has_tracemalloc:
+        tests = [
+            (100, 0.00012302398681640625),
+            (1000, 0.0009813308715820312),
+            (10000, 0.009564399719238281),
+            (100000, 0.09539508819580078),
+            (1000000, 0.9537019729614258),
+            (10000000, 9.536770820617676),
+            (100000000, 95.36745929718018),
+        ]
+        for test_input, expected in tests:
+            test_memory_profiler(test_input, expected)
