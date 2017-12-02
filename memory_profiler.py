c9fcc8faf3c0c62fb7c8f8051baf0bce73afc6cd
Fabian Pedregosa <f@bianp.net>
2017-12-02 09:19:33 -0800

    start of 0.50 development cycle

--- memory_profiler.py
+++ memory_profiler.py
@@ -3,7 +3,7 @@
 # .. we'll use this to pass it to the child script ..
 _CLEAN_GLOBALS = globals().copy()
 
-__version__ = '0.49.0'
+__version__ = '0.50.dev0'
 
 _CMD_USAGE = "python -m memory_profiler script_file.py"
 
