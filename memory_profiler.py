5c1f0c06458bcccfcd4f2088df0ec9a09203d9a5
Fabian Pedregosa <pedregosa@google.com>
2020-01-10 19:43:38 -0500

    version 0.57

--- memory_profiler.py
+++ memory_profiler.py
@@ -3,7 +3,7 @@
 # .. we'll use this to pass it to the child script ..
 _CLEAN_GLOBALS = globals().copy()
 
-__version__ = '0.56.0'
+__version__ = '0.57.0'
 
 _CMD_USAGE = "python -m memory_profiler script_file.py"
 
