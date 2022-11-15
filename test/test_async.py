2199412a3d304d6e44d2189d361a6d55495fbdca
Kyle Altendorf <sda@fstab.net>
2022-11-15 10:22:29 -0500

    asyncio.ensure_future

--- test/test_async.py
+++ test/test_async.py
@@ -15,9 +15,13 @@ async def main():
     task = asyncio.create_task(my_func())
     res = await asyncio.gather(task)
 
+async def main_legacy():
+    future = asyncio.ensure_future(my_func())
+    res = await asyncio.gather(future)
+
 if __name__ == '__main__':
     if sys.version_info >= (3, 7):
         asyncio.run(main())  # main loop
     else:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
+        loop.run_until_complete(main_legacy())
